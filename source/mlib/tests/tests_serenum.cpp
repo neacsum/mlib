6257235a5d7912b57b9073bcb4b784a855eb05ec
Mircea Neacsu <mircea@neacsu.net>
2018-01-09 06:00:54 -0500

    More serenum changes

--- source/mlib/tests/tests_serenum.cpp
+++ source/mlib/tests/tests_serenum.cpp
@@ -2,7 +2,6 @@
 #include <mlib/serenum.h>
 
 using namespace std;
-using namespace mlib;
 
 TEST (SerEnum_Test)
 {
@@ -15,7 +14,7 @@ TEST (SerEnum_Test)
   SerEnum_UsingCreateFile (ports);
   dt = timer.GetTimeInMs ();
   printf ("\nCOM Ports according to CreateFile (%d msec):\n", dt);
-  for (int i = 0; i < ports.size (); i++)
+  for (size_t i = 0; i < ports.size (); i++)
     printf ("COM%d\n", ports[i]);
 
   timer.Start ();
@@ -23,7 +22,7 @@ TEST (SerEnum_Test)
   dt = timer.GetTimeInMs ();
   CHECK_EQUAL (ports.size (), ports1.size());
   printf ("\nCOM Ports according to SetupAPI (%d msec):\n", dt);
-  for (int i = 0; i < ports1.size (); i++)
+  for (size_t i = 0; i < ports1.size (); i++)
     printf ("COM%d - %s\n", ports1[i], names1[i].c_str ());
 
   timer.Start ();
@@ -31,6 +30,6 @@ TEST (SerEnum_Test)
   dt = timer.GetTimeInMs ();
   CHECK_EQUAL (ports.size (), ports2.size ());
   printf ("\nCOM Ports according to Registry (%d msec):\n", dt);
-  for (int i = 0; i < ports2.size (); i++)
+  for (size_t i = 0; i < ports2.size (); i++)
     printf ("COM%d\n", ports2[i]);
 }
