b6d525fe0524c182be24f47d98b7b42691d00e7e
Mircea Neacsu <mircea@neacsu.net>
2018-01-10 12:38:55 -0500

    Compare values in degrees

--- source/mlib/tests/tests_dogbone.cpp
+++ source/mlib/tests/tests_dogbone.cpp
@@ -10,13 +10,13 @@ TEST (dogbone_ok)
 {
   float bucket;
   CHECK_EQUAL (0, dogbone (dims, (float)(15 * M_PI / 180.), (float)(120 * M_PI / 180), &bucket));
-  CHECK_CLOSE (135.8 * M_PI/180, bucket, 0.01);
+  CHECK_CLOSE (135.8, bucket * 180. / M_PI, 0.01);
 }
 
 TEST (dogbone_inverse)
 {
   float dog;
   CHECK_EQUAL (0, invbone (dims, (float)(15 * M_PI / 180.), &dog, (float)(135.8 * M_PI / 180)));
-  CHECK_CLOSE (120 * M_PI / 180, dog, 0.01);
+  CHECK_CLOSE (120., dog * 180. / M_PI, 0.01);
 
 }
