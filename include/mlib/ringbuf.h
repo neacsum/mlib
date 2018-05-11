7fa672cb6d147a8b7d3ecbdd287117f49271b81a
Mircea Neacsu <mircea@neacsu.net>
2018-05-11 10:16:43 -0400

    Reworked ring_buffer class

--- include/mlib/ringbuf.h
+++ include/mlib/ringbuf.h
@@ -8,7 +8,9 @@
 #include <memory>
 #include <vector>
 
+#if __has_include ("defs.h")
 #include "defs.h"
+#endif
 
 #ifdef MLIBSPACE
 namespace MLIBSPACE {
@@ -24,17 +26,36 @@ public:
   class iterator
   {
   public:
+    //Following typedefs must exist to allow instantiation of std::iterator_traits
+    typedef ptrdiff_t difference_type;
+    typedef typename T value_type;
+    typedef typename T& reference;
+    typedef typename T* pointer;
+    typedef std::bidirectional_iterator_tag iterator_category;
+
     ///Default constructor
     iterator () : ring(nullptr), pos(0) {}
 
     ///Dereference operator
-    T& operator *()
+    reference operator *()
+    {
+      return ring->buf[pos];
+    }
+
+    ///Dereference operator (const version)
+    const reference operator *() const
     {
       return ring->buf[pos];
     }
 
     ///Object pointer
-    T* operator ->()
+    pointer operator ->()
+    {
+      return &ring->buf[pos];
+    }
+
+    ///Object pointer (const version)
+    const pointer operator ->() const
     {
       return &ring->buf[pos];
     }
@@ -43,18 +64,14 @@ public:
     iterator operator ++ (int)
     {
       iterator tmp = *this;
-      auto tpos = (pos + 1) % ring->cap;
-      if (tpos != ring->back_idx)
-        pos = tpos;
+      inc_func ();
       return tmp;
     }
 
     ///Increment operator (prefix)
     iterator& operator ++ ()
     {
-      auto tpos = (pos + 1) % ring->cap;
-      if (tpos != ring->back_idx)
-        pos = tpos;
+      inc_func ();
       return *this;
     }
 
@@ -62,16 +79,14 @@ public:
     iterator operator -- (int)
     {
       iterator tmp = *this;
-      if (pos != ring->front_idx)
-        pos = (pos + ring->cap - 1) % ring->cap;
+      dec_func ();
       return tmp;
     }
 
     ///Decrement operator (prefix)
     iterator& operator -- ()
     {
-      if (pos != ring->front_idx)
-        pos = (pos + ring->cap - 1) % ring->cap;
+      dec_func ();
       return *this;
     }
 
@@ -98,12 +113,86 @@ public:
       return *this;
     }
 
+    ///Addition operator
+    iterator operator +(size_t inc) const
+    {
+      iterator tmp = *this;
+      add_func (tmp, inc);
+      return tmp;
+    }
+
+    ///Addition assignment operator
+    iterator& operator +=(size_t inc)
+    {
+      add_func (*this, inc);
+      return *this;
+    }
+
+    ///Subtraction operator
+    iterator operator -(size_t dec) const
+    {
+      iterator tmp = *this;
+      sub_func (tmp, dec);
+      return tmp;
+    }
+
+    ///Subtraction assignment operator
+    iterator& operator -=(size_t dec)
+    {
+      sub_func (*this, dec);
+      return *this;
+    }
+
   private:
     iterator (const ring_buffer* ring_, size_t pos_)
       : ring (ring_)
       , pos (pos_)
     {
     }
+
+    void inc_func ()
+    {
+      if (ring->cap && pos != -1)
+        pos = (pos + 1) % ring->cap;
+      if (pos == ring->back_idx)
+        pos = -1;
+    }
+
+    void dec_func ()
+    {
+      if (ring->cap)
+      {
+        if (pos == -1)
+          pos = (ring->back_idx + ring->cap - 1) % ring->cap;
+        else if (pos != ring->front_idx)
+          pos = (pos + ring->cap - 1) % ring->cap;
+      }
+    }
+
+    void add_func (iterator& it, size_t inc) const
+    {
+      if (ring->cap && pos != -1)
+      {
+        size_t np = it.pos + (inc % ring->cap);
+        if (np >= ring->cap && np >= ring->back_idx + ring->cap)
+          it.pos = -1;
+        else
+          it.pos = np % ring->cap;
+      }
+    }
+
+    void sub_func (iterator& it, size_t dec) const
+    {
+      if (ring->cap)
+      {
+        size_t np = (it.pos == -1 ? ring->back_idx : it.pos) - (dec % ring->cap) + ring->cap;
+        if (np < ring->front_idx)
+          it.pos = ring->front_idx;
+        else
+          it.pos = np % ring->cap;
+      }
+    }
+
     const ring_buffer* ring;
     size_t pos;
     friend class ring_buffer;
@@ -119,19 +208,44 @@ public:
   {
   }
 
+  ///Default constructor
+  ring_buffer ()
+    : cap (0)
+    , front_idx (0)
+    , back_idx (0)
+    , sz (0)
+  {
+  }
+
   ///Copy constructor
   ring_buffer (const ring_buffer& other)
-    : buf (std::unique_ptr<T[]> (new T[other.cap]))
-    , front_idx (other.front_idx)
+    : front_idx (other.front_idx)
     , back_idx (other.back_idx)
     , cap (other.cap)
     , sz (other.sz)
   {
-    for (size_t i = 0; i < sz; i++)
+    if (other.cap)
     {
-      size_t idx = (front_idx + i) % cap;
-      buf[idx] = other.buf[idx];
+      buf = std::unique_ptr<T[]> (new T[other.cap]);
+      for (size_t i = 0; i < sz; i++)
+      {
+        size_t idx = (front_idx + i) % cap;
+        buf[idx] = other.buf[idx];
+      }
     }
+  }
+
+  ///Initializer list constructor
+  ring_buffer (std::initializer_list<T> il)
+    : buf (std::unique_ptr<T[]> (new T[il.size ()]))
+    , cap (il.size ())
+    , front_idx (0)
+    , back_idx (0)
+    , sz (il.size())
+  {
+    size_t i = 0;
+    for (const auto v : il)
+      buf[i++] = std::move(v);
   }
 
   ///Assignment operator
@@ -139,18 +253,46 @@ public:
   {
     if (&rhs != this)
     {
-      buf.reset (new T[rhs.cap]);
       front_idx = rhs.front_idx;
       back_idx = rhs.back_idx;
       cap = rhs.cap;
       sz = rhs.sz;
-      for (size_t i = 0; i < sz; i++)
+      if (rhs.cap)
       {
-        size_t idx = (front_idx + i) % cap;
-        buf[idx] = rhs.buf[idx];
+        buf.reset (new T[rhs.cap]);
+        for (size_t i = 0; i < sz; i++)
+        {
+          size_t idx = (front_idx + i) % cap;
+          buf[idx] = rhs.buf[idx];
+        }
       }
+      else
+        buf.reset ();
     }
     return *this;
+  }
+
+  ///Equality operator
+  bool operator == (const ring_buffer<T>& other) const
+  {
+    if (cap == other.cap && sz == other.sz)
+    {
+      for (size_t i = 0; i < sz; i++)
+      {
+        size_t idx1 = (front_idx + i) % cap;
+        size_t idx2 = (other.front_idx + i) % other.cap;
+        if (buf[idx1] != other.buf[idx2])
+          return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  ///Inequality operator
+  bool operator != (const ring_buffer<T>& other) const
+  {
+    return !operator ==(other);
   }
 
   /// Vector conversion operator
@@ -166,6 +308,9 @@ public:
   ///Inserts new element in buffer
   void push_back (const T& item) 
   {
+    if (!cap)
+      return; //container not allocated
+
     if (sz && back_idx == front_idx)
       front_idx = (front_idx + 1) % cap;
     else
@@ -174,7 +319,19 @@ public:
     back_idx = (back_idx + 1) % cap;
   }
 
-  ///Removes oldest element from buffer
+  ///Return an iterator pointing to first (oldest) element in buffer
+  iterator begin () const
+  {
+    return iterator (this, front_idx);
+  }
+
+  ///Return an iterator pointing past the last (newest) element in buffer
+  iterator end () const
+  {
+    return iterator (this, (size_t)(-1));
+  }
+
+  ///Remove oldest element from buffer
   void pop_front ()
   {
     if (sz)
@@ -184,50 +341,63 @@ public:
     }
   }
 
-  /*!
-    Returns an iterator pointing to first (oldest) element in buffer.
-    This iterator can only be incremented to access subsequent (newer) elements.
-  */
-  iterator front ()
+  /// Return a reference to first (oldest) element in buffer.
+  T& front ()
   {
-    return iterator (this, front_idx);
+    return buf[front_idx];
   }
 
-  /*!
-    Returns an iterator pointing to last (newest) element in buffer.
-    This iterator can only be decremented to access previous (older) elements.
-  */
-  iterator back ()
+  /// Return reference to last (newest) element in buffer.
+  T& back ()
   {
-    return iterator (this, (back_idx + cap - 1) % cap);
+    return buf[(back_idx + cap - 1) % cap];
   }
 
-  ///Removes all elements from buffer
+  ///Remove all elements from buffer
   void clear (void)
   {
     front_idx = back_idx;
     sz = 0;
   }
 
-  ///Returns true if buffer is empty
+  ///Return true if buffer is empty
   bool empty (void) 
   {
     return (sz == 0);
   }
 
-  ///Returns true if buffer is full
+  ///Return true if buffer is full
   bool full (void)
   {
     return (sz == cap);
   }
 
-  ///Returns buffer maximum size
-  size_t capacity (void) 
+  ///Return maximum buffer size
+  size_t capacity (void)
   {
     return cap;
   };
 
-  ///Returns current number of elements in buffer
+  ///(Re)allocate buffer with a different capacity
+  void resize (size_t new_cap)
+  {
+    std::unique_ptr<T[]> newbuf (new_cap ? new T[new_cap] : nullptr);
+    if (new_cap)
+    {
+      if (sz >= new_cap)
+        sz = new_cap;
+      for (size_t i = 0; i < sz; i++)
+        newbuf[i] = buf[(front_idx + i) % cap];
+    }
+    else
+      sz = 0;
+    cap = new_cap;
+    buf.swap(newbuf);
+    front_idx = 0;
+    back_idx = cap ? (front_idx + sz) % cap : 0;
+  }
+
+  ///Return number of elements in buffer
   size_t size () const
   {
     return sz;
@@ -236,6 +406,7 @@ public:
 private:
   std::unique_ptr<T[]> buf;
   size_t front_idx, back_idx, cap, sz;
+
 };
 
 #ifdef MLIBSPACE
--- tests/source/tests_ringbuffer.cpp
+++ tests/source/tests_ringbuffer.cpp
@@ -1,275 +1,402 @@
 #include <utpp/utpp.h>
 #include <mlib/ringbuf.h>
 #include <vector>
+#include <iostream>
 
 #ifdef MLIBSPACE
 using namespace MLIBSPACE;
 #endif
 
+///Used to count ctor/dtor calls
+struct counted_int {
+  counted_int (int i=0) : val (i), inst (counter++) {
+#ifdef COUNTER_DETAILS
+    std::cout << "ctor int(" << inst << ")=" << val << "\n";
+#endif
+  }
+  counted_int (const counted_int& other) : val (other.val), inst (counter++) {
+#ifdef COUNTER_DETAILS
+    std::cout << "copy ctor int(" << inst << ")=" << val << " copied from " << other.inst << "\n";
+#endif
+  }
+  counted_int& operator =(const counted_int& rhs) {
+    val = rhs.val;
+    return *this;
+  }
+  bool operator == (const counted_int& rhs) {
+    return val == rhs.val;
+  }
+  ~counted_int () {
+    --counter; 
+#ifdef COUNTER_DETAILS
+    std::cout << "dtor int(" << inst << ")=" << val << " remain=" << counter << "\n";
+#endif
+  }
+
+  operator int () { return val; }
+
+  int val, inst;
+  static int counter;
+};
+
+int counted_int::counter = 0;
 
 SUITE (RingBuffer)
 {
-  const int BUFSZ = 10;
-
-  struct make_intbuf {
-    ring_buffer <int> intbuf;
-    make_intbuf () : intbuf (BUFSZ) {}
-    void fill (int sz)
-    {
-      for (int i = 0; i < sz; i++)
-        intbuf.push_back (i);
-    }
-  };
-
-  TEST_FIXTURE (make_intbuf, Constructor)
+  TEST (ctor_dtor1)
   {
-    intbuf.push_back (1);
-
-    ring_buffer<int>::iterator it = intbuf.front ();
-    int front_val = *it;
-    CHECK_EQUAL (1, front_val);
+    counted_int::counter = 0;
+    {
+      // VERIFY: ring_buffer constructor with a size argument calls 
+      // object's default constructor once for each object in container.
+      ring_buffer <counted_int> testbuf (10);
+      CHECK_EQUAL (10, counted_int::counter);
+    }
+    // VERIFY: when ring buffer goes out of scope all elements are destroyed.
+    CHECK_EQUAL (0, counted_int::counter);
   }
 
-  TEST_FIXTURE (make_intbuf, Full)
+  TEST(ctor_dtor2)
   {
-    for (int i = 0; i < BUFSZ; i++)
-    {
-      CHECK (!intbuf.full ());
-      intbuf.push_back (i);
-      CHECK_EQUAL (i + 1, intbuf.size ());
-    }
-    CHECK (intbuf.full ());
-
-    for (int i = 0; i < 5; i++)
-    {
-      intbuf.push_back (i);
-      CHECK (intbuf.full ());
-    }
+    counted_int::counter = 0;
+    ring_buffer <counted_int> emptybuf;
+    // VERIFY: default constructor doesn't call object constructor.
+    CHECK_EQUAL (0, counted_int::counter);
   }
 
-  TEST_FIXTURE (make_intbuf, Empty)
+  TEST (copy_ctor1)
   {
-    CHECK (intbuf.empty ());
-
-    //fill and overfill buffer
-    for (int i = 0; i < BUFSZ+5; i++)
-    {
-      intbuf.push_back (i);
-      CHECK (!intbuf.empty ());
-    }
-
-    for (int i = 0; i < BUFSZ; i++)
-    {
-      CHECK (!intbuf.empty ());
-      intbuf.pop_front ();
-    }
-    CHECK (intbuf.empty ());
+    counted_int::counter = 0;
+    ring_buffer <counted_int> testbuf (10);
+    ring_buffer <counted_int> otherbuf (testbuf);
+    
+    // VERIFY: object constructor called for each element in container
+    CHECK_EQUAL (20, counted_int::counter);
   }
 
-  TEST_FIXTURE (make_intbuf, Size)
+  TEST (copy_ctor2)
   {
-    //until buffer is full size increases with each push
-    for (int i = 0; i < BUFSZ; i++)
-    {
-      CHECK_EQUAL (i, intbuf.size ());
-      intbuf.push_back (i);
-    }
+    counted_int::counter = 0;
+    ring_buffer <counted_int> testbuf;
+    ring_buffer <counted_int> otherbuf (testbuf);
 
-    //size doesn't increase when buffer is full
-    for (int i = 0; i < 5; i++)
-    {
-      CHECK_EQUAL (BUFSZ, intbuf.size ());
-      intbuf.push_back (i);
-    }
-
-    //size goes down when popping elements
-    for (int i = BUFSZ; i > 0; i--)
-    {
-      CHECK_EQUAL (i, intbuf.size ());
-      intbuf.pop_front ();
-    }
-    CHECK (intbuf.empty ());
+    // VERIFY: object constructor not called if source container is empty
+    CHECK_EQUAL (0, counted_int::counter);
   }
 
-  TEST_FIXTURE (make_intbuf, front_iterator)
+  TEST (copy_ctor3)
   {
-    fill (BUFSZ);
+    ring_buffer <int> testbuf(10);
+    for (int i = 1; i <= 10; i++)
+      testbuf.push_back (i);
 
-    //postfix operator
-    auto it = intbuf.front ();
-    int val;
-    for (int i = 0; i < BUFSZ; i++)
-    {
-      val = *it++;
-      CHECK_EQUAL (i, val);
-    }
+    ring_buffer <int> otherbuf (testbuf);
 
-    //cannot increment past back
-    for (int i = 0; i < 5; i++)
-    {
-      val = *it++;
-      CHECK_EQUAL (BUFSZ-1, val);
-    }
-
-    //repeat for prefix operator
-    it = intbuf.front ();
-    for (int i = 0; i < BUFSZ; i++)
-    {
-      val = *it;
-      CHECK_EQUAL (i, val);
-      ++it;
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-      ++it;
-      val = *it;
-      CHECK_EQUAL (BUFSZ - 1, val);
-    }
+    // VERIFY: copied objects are equal
+    CHECK (testbuf == otherbuf);
   }
 
-  TEST_FIXTURE (make_intbuf, back_iterator)
+  TEST (copy_ctor4)
   {
-    fill (BUFSZ);
+    ring_buffer <int> testbuf;
+    ring_buffer <int> otherbuf (testbuf);
 
-    auto it = intbuf.back ();
-    int val;
-    for (int i = BUFSZ - 1; i >= 0; i--)
-    {
-      val = *it--;
-      CHECK_EQUAL (i, val);
-    }
-
-    //cannot decrement past front
-    for (int i = 0; i < 5; i++)
-    {
-      val = *it--;
-      CHECK_EQUAL (0, val);
-    }
-
-    //repeat for prefix operator
-    it = intbuf.back ();
-    for (int i = BUFSZ - 1; i >= 0; i--)
-    {
-      val = *it;
-      CHECK_EQUAL (i, val);
-      --it;
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-      val = *--it;
-      CHECK_EQUAL (0, val);
-    }
+    // VERIFY: empty copied containers are equal
+    CHECK (testbuf == otherbuf);
   }
 
-  //Same test as front_iterator but with buffer half full
-  TEST_FIXTURE (make_intbuf, front_iterator1)
+  TEST (assignment_op1)
   {
-    fill (BUFSZ / 2);
+    counted_int::counter = 0;
+    ring_buffer <counted_int> testbuf (10);
+    for (int i = 1; i <= 10; i++)
+      testbuf.push_back (i);
 
-    //postfix operator
-    auto it = intbuf.front ();
-    int val;
-    for (int i = 0; i < BUFSZ/2; i++)
-    {
-      val = *it++;
-      CHECK_EQUAL (i, val);
-    }
+    ring_buffer <counted_int> otherbuf;
+    testbuf = otherbuf;
 
-    //cannot increment past back
-    for (int i = 0; i < 5; i++)
-    {
-      val = *it++;
-      CHECK_EQUAL (BUFSZ/2 - 1, val);
-    }
-
-    //repeat for prefix operator
-    it = intbuf.front ();
-    for (int i = 0; i < BUFSZ/2; i++)
-    {
-      val = *it;
-      CHECK_EQUAL (i, val);
-      ++it;
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-      ++it;
-      val = *it;
-      CHECK_EQUAL (BUFSZ/2 - 1, val);
-    }
+    // VERIFY: assigned objects are equal
+    CHECK (testbuf == otherbuf);
   }
 
-  TEST_FIXTURE (make_intbuf, Comparison)
+  TEST (assignment_op2)
   {
-    fill (BUFSZ);
-    std::vector<int> vi, vo;
+    ring_buffer <counted_int> testbuf (10);
+    for (int i = 1; i <= 10; i++)
+      testbuf.push_back (i);
 
-    for (int i = 0; i < BUFSZ; i++)
-      vi.push_back (i);
+    ring_buffer <counted_int> emptybuf;
+    testbuf = emptybuf;
 
-    // a nice template to iterate the whole buffer
-    auto it = intbuf.front ();
-    do 
-    {
-      vo.push_back (*it);
-    } while (it != it++);
+    // VERIFY: assigning an empty container invokes object destructor for
+    // all objects currently in container
+    CHECK_EQUAL (0, counted_int::counter);
 
-    CHECK (vi == vo);
+    // VERIFY: assigned container is empty
+    CHECK (testbuf.empty ());
   }
 
-  TEST_FIXTURE (make_intbuf, Copy_constructor)
+  TEST (assignment_op3)
   {
-    fill (BUFSZ + 5);
+    ring_buffer <counted_int> empty1;
+    ring_buffer <counted_int> empty2;
+    empty1 = empty2;
 
-    ring_buffer <int>copybuf (intbuf);
-
-    auto it = intbuf.front ();
-    auto it1 = copybuf.front ();
-    do
-    {
-      CHECK_EQUAL (*it, *it1++);
-    } while (it != it++);
+    // VERIFY: assigning empty to empty is OK
+    CHECK (empty1.empty() && empty2.empty());
   }
 
-  TEST_FIXTURE (make_intbuf, Assignment_operator)
+  TEST (assignment_op4)
   {
-    fill (BUFSZ + 5);
-    ring_buffer <int>otherbuf (5);
-
-    otherbuf = intbuf;
-
-    auto it = intbuf.front ();
-    auto it1 = otherbuf.front ();
-    do
+    counted_int::counter = 0;
     {
-      CHECK_EQUAL (*it, *it1++);
-    } while (it != it++);
+      ring_buffer<counted_int> b1 ({ 100, 101, 102 });
+      ring_buffer<counted_int> b2;
+
+      b2 = b1;
+      //VERIFY: new objects are constructed
+      CHECK_EQUAL (6, counted_int::counter);
+
+      //VERIFY: objects are equal
+      CHECK (b1 == b2);
+    }
+
+    //VERIFY: all objects are destructed when containers are destructed
+    CHECK (counted_int::counter == 0);
   }
 
-  TEST_FIXTURE (make_intbuf, vector_operator)
+  TEST (equal_op)
   {
-    std::vector<int> v;
+    ring_buffer<int> buf1 (10);
+    ring_buffer<int> buf2 (10);
+    ring_buffer<int> buf3 (5);
 
-    //half-full ring buffer
-    fill (BUFSZ / 2);
+    // VERIFY: empty container equals empty container of same size
+    CHECK (buf1 == buf2);
 
-    v = intbuf;
+    // VERIFY: containers of different sizes are not equal even if both are empty
+    CHECK (buf1 != buf3);
 
-    CHECK_EQUAL (v.size (), intbuf.size ());
-    for (int i = 0; i < BUFSZ / 2; i++)
-      CHECK_EQUAL (i, v[i]);
+    for (int i = 1; i <= 10; i++)
+    {
+      buf1.push_back (i);
+      buf2.push_back (i);
 
-    //overfill ring buffer
-    for (int i = 0; i < BUFSZ; i++)
-      intbuf.push_back (i*10);
+      // VERIFY: containers are equal while filling up
+      CHECK (buf1 == buf2);
+    }
 
-    v = intbuf;
+    for (int i = 1; i <= 10; i++)
+      buf2.push_back (i);
 
-    CHECK_EQUAL (v.size (), intbuf.size ());
-    for (int i = 0; i < BUFSZ; i++)
-      CHECK_EQUAL (i*10, v[i]);
+    // VERIFY: containers are equal after one of them overflows
+    CHECK (buf1 == buf2);
+  }
+
+  TEST (push_back)
+  {
+    ring_buffer<int> testbuf (10);
+    testbuf.push_back (100);
+
+    // VERIFY: size is increased after push_back
+    CHECK_EQUAL (1, testbuf.size ());
+
+    // VERIFY: back element is the one pushed
+    CHECK_EQUAL (100, testbuf.back ());
+
+    auto bptr = testbuf.begin ();
+
+    // VERIFY: begin points to the object pushed
+    CHECK_EQUAL (100, *bptr);
+
+    auto eptr = testbuf.end ();
+
+    // VERIFY: end points one after the object pushed
+    CHECK_EQUAL (100, *--eptr);
+  }
+
+  TEST (begin)
+  {
+    ring_buffer<int> testbuf (10);
+    testbuf.push_back (100);
+    testbuf.push_back (101);
+
+    auto iter = testbuf.begin ();
+
+    // VERIFY: begin points to first object pushed
+    CHECK_EQUAL (100, *iter);
+
+    iter == --testbuf.begin ();
+
+    // VERIFY: begin iterator doesn't change when decremented
+    CHECK (iter == testbuf.begin());
+
+    iter--;
+
+    // VERIFY: postfix decrement doesn't change begin iterator
+    CHECK (iter == testbuf.begin());
+  }
+
+  TEST (end)
+  {
+    ring_buffer<int> testbuf (10);
+    testbuf.push_back (100);
+    testbuf.push_back (101);
+
+    auto last = --testbuf.end ();
+
+    // VERIFY: pointer to last object becomes end after increment
+    CHECK (++last == testbuf.end ());
+
+    last--;
+
+    // VERIFY: postfix decrement brings pointer to last object pushed
+    CHECK_EQUAL (101, *last);
+
+    last++;
+
+    // VERIFY: postfix increment brings pointer to end
+    CHECK (last == testbuf.end ());
+
+    // VERIFY: prefix increment doesn't change end pointer
+    CHECK (++last == testbuf.end ());
+
+    last++;
+
+    // VERIFY: postfix increment doesn't change end pointer
+    CHECK (last == testbuf.end ());
+  }
+
+  TEST (addition)
+  {
+    const ring_buffer<int> testbuf ({100, 101, 102});
+
+    auto it1 = testbuf.begin();
+    
+    // VERIFY: increment and addition give the same result
+    auto it2 = it1++ + 1;
+    CHECK (it1 == it2);
+
+    // VERIFY: addition works
+    int v = *(testbuf.begin() + 2);
+    CHECK_EQUAL (102, v);
+
+    // VERIFY: addition stops at end
+    it2 = it1 + (testbuf.size ()-1);
+    CHECK (it2 == std::end (testbuf));
+  }
+
+  TEST (subtraction)
+  {
+    const ring_buffer<int> testbuf ({ 100, 101, 102 });
+
+    auto it1 = ++testbuf.begin ();
+
+    // VERIFY: decrement and subtraction give same result
+    auto it2 = it1-- - 1;
+    CHECK (it1 == it2);
+
+    // VERIFY: subtraction works
+    int v = *(testbuf.end () - 2);
+    CHECK_EQUAL (101, v);
+  }
+
+  TEST (resize)
+  {
+    counted_int::counter = 0;
+    ring_buffer<counted_int> testbuf ({ 100, 101, 102 });
+
+    testbuf.resize (5);
+    //VERIFY: number of objects created equals new container size
+    CHECK_EQUAL (5, counted_int::counter);
+
+    //VERIFY: resize can deallocate container
+    testbuf.resize (0);
+    CHECK_EQUAL (0, testbuf.capacity ());
+    CHECK_EQUAL (0, counted_int::counter);
+
+    //VERIFY: resize can reallocate container
+    testbuf.resize (5);
+    CHECK_EQUAL (5, testbuf.capacity ());
+    CHECK_EQUAL (5, counted_int::counter);
+  }
+
+  TEST (full)
+  {
+    ring_buffer<int> testbuf ({ 100, 101, 102 });
+
+    //VERIFY: container created from initializer list is full (size == number of elements in list)
+    CHECK (testbuf.full ());
+
+    //VERIFY: container remains full after pushing more objects
+    testbuf.push_back (103);
+    CHECK (testbuf.full ());
+
+    //VERIFY: not full after popping one object
+    testbuf.pop_front ();
+    CHECK (!testbuf.full ());
+  }
+
+  TEST (empty)
+  {
+    ring_buffer<int> testbuf;
+
+    //VERIFY: unallocated container is empty
+    CHECK (testbuf.empty ());
+
+    //VERIFY: newly allocated container is still empty
+    testbuf.resize (3);
+    CHECK (testbuf.empty ());
+
+    //VERIFY: container not empty after inserting an object
+    testbuf.push_back (100);
+    CHECK (!testbuf.empty ());
+
+    //VERIFY: container becomes empty after popping (last) object
+    testbuf.pop_front ();
+    CHECK (testbuf.empty ());
+  }
+
+  TEST (size)
+  {
+    ring_buffer<int> testbuf ({ 100, 101, 102 });
+
+    //VERIFY: container created from initializer list has size and capacity == number of elements in list
+    CHECK_EQUAL (3, testbuf.size ());
+    CHECK_EQUAL (3, testbuf.capacity ());
+
+    //VERIFY: inserting more objects doesn't increase size
+    testbuf.push_back (103);
+    CHECK_EQUAL (3, testbuf.size ());
+
+    //VERIFY: size decreases when objects are popped
+    testbuf.pop_front ();
+    CHECK_EQUAL (2, testbuf.size ());
+    testbuf.pop_front ();
+    CHECK_EQUAL (1, testbuf.size ());
+    testbuf.pop_front ();
+    CHECK_EQUAL (0, testbuf.size ());
+  }
+
+  TEST (vector_op)
+  {
+    const ring_buffer<int> testbuf ({ 100, 101, 102 });
+    const std::vector<int> expected ({ 100, 101, 102 });
+    std::vector<int> actual;
+    actual = testbuf;
+    CHECK (expected == actual);
+  }
+
+  TEST (find_algorithm)
+  {
+    const ring_buffer<int> testbuf ({ 100, 101, 102 });
+
+    auto it = std::find (testbuf.begin (), testbuf.end (), 101);
+
+    //VERIFY: found second element
+    CHECK (testbuf.begin () + 1 == it);
+
+    *it = 104;
   }
 }
 
