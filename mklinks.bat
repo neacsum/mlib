c8533479918fdc2a461a4661d1f5d15a85559c16
Mircea Neacsu <mircea@neacsu.net>
2018-10-09 10:29:36 -0400

    Added mklinks.bat

---
+++ mklinks.bat
@@ -0,0 +1,11 @@
+@echo off
+rem
+rem  Create symbolic links for included projects and output directories.
+rem  Environment variable DEV_ROOT points to the root of development tree.
+rem
+rem  This script should be run as administrator
+rem
+
+pushd "%~dp0"
+mklink /d lib %DEV_ROOT%\lib
+popd
