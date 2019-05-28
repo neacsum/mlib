23bfc9e23d28be8becc0a083da83fb4f8cc9e478
Mircea Neacsu <mircea@neacsu.net>
2019-05-28 10:14:51 -0400

    Added master include file

---
+++ include/mlib/mlib.h
@@ -0,0 +1,30 @@
+#pragma once
+/*!
+  \file MLIB.H Uber-include file for mlib library
+
+  (c) Mircea Neacsu 2019
+*/
+
+#include "base64.h"
+#include "basename.h"
+#include "bitstream.h"
+#include "border.h"
+#include "chull.h"
+#include "convert.h"
+#include "crc32.h"
+#include "firewall.h"
+#include "jbridge.h"
+#include "log.h"
+#include "md5.h"
+#include "mutex.h"
+#include "nmea.h"
+#include "options.h"
+#include "profile.h"
+#include "ringbuf.h"
+#include "semaphore.h"
+#include "serenum.h"
+#include "shmem.h"
+#include "sqlitepp.h"
+#include "stopwatch.h"
+#include "trace.h"
+#include "wtimer.h"
