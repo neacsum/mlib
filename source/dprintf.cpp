e1cb4c528ebd657d19bbc39e3046b05b058ddaa3
Mircea Neacsu <mircea@neacsu.net>
2020-08-17 20:40:01 -0400

    Serialize dprintf function calls

--- source/dprintf.cpp
+++ source/dprintf.cpp
@@ -1,7 +1,7 @@
 /*!
   \file dprintf.cpp Implementation of dprintf() function
 
-  (c) Mircea Neacsu 1999-2000. All rights reserved.
+  (c) Mircea Neacsu 1999-2020. All rights reserved.
 */
 
 #include <mlib/dprintf.h>
@@ -9,12 +9,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+static INIT_ONCE dpr_init = INIT_ONCE_STATIC_INIT;
+static CRITICAL_SECTION dpr_cs;
+
+//Initialization function executed by InitOnceExecuteOnce
+BOOL CALLBACK dpr_inifun (PINIT_ONCE, PVOID, PVOID*)
+{
+  InitializeCriticalSection (&dpr_cs);
+  return true;
+}
+
 /*!
   printf style function writes messages using OutputDebugString.
   Message length is limited to MAX_DPRINTF_CHARS characters.
 
   \param fmt print format
   \return true if successful, false otherwise
+
+  \note Function is thread-safe. Calls from different threads are serialized by
+  a critical section object.
 */
 
 bool dprintf (const char *fmt, ...)
@@ -23,6 +36,7 @@ bool dprintf (const char *fmt, ...)
   size_t sz;
   int r;
   wchar_t *out;
+  InitOnceExecuteOnce (&dpr_init, dpr_inifun, NULL, NULL);
 
   va_list params;
   va_start (params, fmt);
@@ -40,7 +54,9 @@ bool dprintf (const char *fmt, ...)
   if (wsz && (out = (wchar_t*)malloc (wsz*sizeof (wchar_t))))
   {
     MultiByteToWideChar (CP_UTF8, 0, buffer, -1, out, wsz);
+    EnterCriticalSection (&dpr_cs);
     OutputDebugString (out);
+    LeaveCriticalSection (&dpr_cs);
     free (out);
     return true;
   }
