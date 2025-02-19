c7e05f94bf0baf796dc83d7125486e2a959ec0b1
Mircea Neacsu <mircea@neacsu.net>
2025-02-19 11:29:55 -0500

    mlib::sock - added SOCKET conversion operator wol - added destination option

--- examples/wol/wol.cpp
+++ examples/wol/wol.cpp
@@ -10,10 +10,17 @@
   For more information see: https://en.wikipedia.org/wiki/Wake-on-LAN
 
   Usage:
-    wol <mac_address>
+    wol [-h|--help] [-d <host_ip>] <mac_address>
 
   The MAC address can be in format xx:xx:xx:xx:xx:xx or xx-xx-xx-xx-xx-xx
 */
+
+using mlib::OptParser;
+
+ OptParser cmds{
+   "d: host \t destination or broadcast IP address", 
+   "h|help \t show help message"
+ };
 
 // parse MAC address
 bool parse_mac (const char* str, unsigned char* mac)
@@ -36,43 +43,80 @@ bool parse_mac (const char* str, unsigned char* mac)
   return (i == 6);
 }
 
+void help ()
+{
+  std::cout 
+    << "Wake-on-LAN utility. This program sends a magic packet to a "
+       "destination device that supports WOL.\n"
+       "Usage: \n"
+    << '\t' << cmds.appname () << " [options] <mac_address> \n\n"
+    << "Valid options are:\n"
+    << "\t" << cmds.synopsis () << "\n\n"
+    << "Where:\n"
+    << cmds.description () << '\n'
+    << "<mac_address> can be 'xx:xx:xx:xx:xx:xx' or 'xx-xx-xx-xx-xx-xx'\n";
+}
+
+
 int main (int argc, char** argv)
 {
-  auto app = std::filesystem::path (argv[0]).stem ().string ();
-  if (argc < 2)
+  //parse command line
+  int argmac;
+  auto ret = cmds.parse (argc, argv, &argmac);
+  if (argc < 2 || cmds.hasopt('h'))
   {
-    std::cout << "Wake-on-LAN utility. This program sends a magic packet to a "
-      "destination device that support WOL.\n"
-      "Usage: \n" 
-      << '\t' << app  << " <mac_address> \n\n"
-      << "where <mac_address> can be 'xx:xx:xx:xx:xx:xx' or 'xx-xx-xx-xx-xx-xx'\n";
+    help ();
     exit (0);
   }
 
-  unsigned char mac[6];
-
-  if (!parse_mac (argv[1], mac))
+  if (ret != 0 || argmac != argc - 1)
   {
-    std::cerr << app << " -- Invalid MAC address: " << argv[1] << std::endl;
+    std::cout << "Syntax error. Valid options are:\n" << cmds.synopsis () << '\n';
     exit (1);
   }
 
+  //parse MAC address
+  unsigned char mac[6];
+  if (!parse_mac (argv[argmac], mac))
+  {
+    std::cerr << cmds.appname() << " -- Invalid MAC address: " << argv[argmac] << std::endl;
+    exit (1);
+  }
+
+  const mlib::sock s (SOCK_DGRAM);
+
+  // set destination or broadcast address
+  mlib::inaddr destination;
+  DWORD sz;
+  WSAIoctl (s, SIO_GET_BROADCAST_ADDRESS, NULL, 0, &destination, sizeof (destination), &sz, NULL, NULL);
+  destination.port (9);
+
+  if (cmds.hasopt ('d'))
+  {
+    std::string dest_str;
+    cmds.getopt ('d', dest_str);
+    if (destination.host (dest_str) != mlib::erc::success)
+    {
+      std::cerr << cmds.appname () << " -- Invalid destination address: " << dest_str << std::endl;
+      exit (1);
+    }
+  }
   //create magic packet
   unsigned char magic[102];
   int i = 0;
   while (i < 6)
     magic[i++] = 0xff;
   
-  while (i < 102)
+  while (i < sizeof(magic))
   {
     memcpy (&magic[i], mac, sizeof (mac));
     i += sizeof (mac);
   }
 
   //send magic packet
-  mlib::sock s (SOCK_DGRAM);
   s.broadcast (true);
-  s.sendto (mlib::inaddr (INADDR_BROADCAST, 9), magic, sizeof (magic));
+  std::cout << "Sending magic packet to " << destination << std::endl;
+  s.sendto (destination, magic, sizeof (magic));
 
   return 0;
 }
